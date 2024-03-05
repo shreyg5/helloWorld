65d8251786865887f6bdaf7b8e7a697272089ad9
shreyg5 <shreyg@terpmail.umd.edu>
2024-03-05 17:42:27 -0500

    Added the about-css route for HTML page.

--- app.py
+++ app.py
@@ -7,13 +7,21 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World from Shreya Gupta! This is my first code change.'
 
+
 @app.route('/hello')
 def hello():
     return render_template('hello.html')
 
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/about-css')
+def about_css():
+    return render_template('about-css.html')
+
 
 if __name__ == '__main__':
     app.run()
